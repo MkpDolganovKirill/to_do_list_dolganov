7c57ae6a42cdc1a3a039bb9e2f65a40d56c01457
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-03-25 17:57:12 +0300

    isEdit don't use on the server. Work with the server has been normal

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
-let allTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let allTasks = [];
 let valueInput = '';
 let input = null;
 
-window.onload = () => {
+window.onload = async () => {
 	input = document.getElementById('input-task');
 	input.addEventListener('change', updateValue);
+	const resp = await fetch('http://localhost:8000/allTasks', {
+		method: 'GET'
+	})
+	let result = await resp.json();
+	if (allTasks) {
+		allTasks = result.data;
+		allTasks.forEach(element => {
+			element.isEdit = false;
+		});
+	}
+	saveToSessionStorage();
 	render();
 }
 
-const onClickButton = () => {
+const onClickButton = async () => {
 	if (valueInput !== '') {
 		allTasks.push({
 			text: valueInput,
-			isChecked: false,
+			isCheck: false,
 			isEdit: false
 		});
+
+		const resp = await fetch('http://localhost:8000/createTask', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+				'Access-Control-Allow-Origin': '*'
+			},
+			body: JSON.stringify({
+				text: valueInput,
+				isCheck: false
+			})
+		});
+		let result = await resp.json();
+		allTasks = result.data;
+		if (allTasks) {
+			allTasks = result.data;
+			allTasks.forEach(element => {
+				element.isEdit = false;
+			});
+		};
 	} else {
 		alert("You can't add empty task!");
 	};
-	saveToLocalStorage();
+
+	
+
+	saveToSessionStorage();
 	valueInput = '';
 	input.value = '';
 	render();
 };
 
 const deleteAllTAsksButton = () => {
+	allTasks.forEach(async (element) => {
+		await fetch(`http://localhost:8000/deleteTask?id=${element.id}`, {
+			method: 'DELETE'
+		});
+	});
 	allTasks.splice(0, allTasks.length);
-	saveToLocalStorage();
 	render();
 };
 
@@ -36,10 +74,10 @@ const updateValue = (event) => {
 
 render = () => {
 	sortedTasks();
-	saveToLocalStorage();
+	saveToSessionStorage();
 	const content = document.getElementById('content-page');
 	while (content.firstChild) {
-		content.removeChild(content.firstChild)
+		content.removeChild(content.firstChild);
 	};
 	allTasks.map((item, index) => {
 		const container = document.createElement('div');
@@ -47,7 +85,7 @@ render = () => {
 		container.className = 'container-list';
 		const checkbox = document.createElement('input');
 		checkbox.type = 'checkbox';
-		checkbox.checked = item.isChecked;
+		checkbox.checked = item.isCheck;
 
 		checkbox.onclick = () => {
 			onChangeCheckBox(index);
@@ -56,7 +94,7 @@ render = () => {
 		container.appendChild(checkbox);
 		const text = document.createElement('p');
 		text.innerText = item.text;
-		text.className = item.isChecked ? 'text-task done-text' : 'text-task';
+		text.className = item.isCheck ? 'text-task done-text' : 'text-task';
 		item.isEdit ? text.classList.add('edit-text') : '';
 		text.id = `text=${index}`;
 
@@ -66,7 +104,7 @@ render = () => {
 
 		const imageEdit = document.createElement('img');
 		imageEdit.src = 'icons/edit.svg';
-		imageEdit.className = item.isEdit || item.isChecked ? 'icons-hidden' : 'icons';
+		imageEdit.className = item.isCheck ? 'icons-hidden' : 'icons';
 
 		const imageCancel = document.createElement('img');
 		imageCancel.src = 'icons/close.svg';
@@ -85,18 +123,16 @@ render = () => {
 		};
 
 		imageEdit.onclick = () => {
-			editImage(index);
+			editImageClick(index);
 		};
 
 		imageCancel.onclick = () => {
-			editImage(index);
+			editImageClick(index);
 		};
 
 		imageComplete.onclick = () => {
 			saveEditFromInput(index, inputEdit);
 		};
-
-		
 
 		container.appendChild(text);
 		container.appendChild(inputEdit);
@@ -108,43 +144,72 @@ render = () => {
 	})
 }
 
-const onChangeCheckBox = (index) => {
-	allTasks[index].isChecked = !allTasks[index].isChecked;
+const onChangeCheckBox = async (index) => {
+	allTasks[index].isCheck = !allTasks[index].isCheck;
+	const resp = await fetch(`http://localhost:8000/updateTask`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+			'Access-Control-Allow-Origin': '*'
+		},
+		body: JSON.stringify({
+			id: allTasks[index].id,
+			text: allTasks[index].text,
+			isCheck: allTasks[index].isCheck
+		})
+	});
 	render();
 };
 
-const editImage = (index) => {
+const editImageClick = (index) => {
 	allTasks[index].isEdit = !allTasks[index].isEdit;
 	render();
 
 };
 
-const saveEditFromInput = (index, inputEdit) => {
+const saveEditFromInput = async (index, inputEdit) => {
 	if (inputEdit.value !== '') {
 		allTasks[index].text = inputEdit.value;
+		const resp = await fetch(`http://localhost:8000/updateTask`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8',
+				'Access-Control-Allow-Origin': '*'
+			},
+			body: JSON.stringify({
+				id: allTasks[index].id,
+				text: inputEdit.value,
+				isCheck: allTasks[index].isCheck
+			})
+		});
+
 		allTasks[index].isEdit = !allTasks[index].isEdit;
 	} else {
 		deleteElement(index);
-	}
+	};
 	render();
 };
 
-const deleteElement = (index) => {
-	allTasks.splice(index, 1);
+const deleteElement = async (index) => {
+	const resp = await fetch(`http://localhost:8000/deleteTask?id=${allTasks[index].id}`, {
+		method: 'DELETE'
+	});
+	let result = await resp.json();
+	allTasks = result.data;
 	render();
 };
 
 const sortedTasks = () => {
 	allTasks.sort((a, b) => {
-		if (a.isChecked < b.isChecked) {
+		if (a.isCheck < b.isCheck) {
 			return -1;
-		} else if (a.isChecked > b.isChecked) {
+		} else if (a.isCheck > b.isCheck) {
 			return 1;
 		}
 		return 0;
 	});
 };
 
-const saveToLocalStorage = () => {
-	localStorage.setItem('tasks', JSON.stringify(allTasks));
+const saveToSessionStorage = () => {
+	sessionStorage.setItem('tasks', JSON.stringify(allTasks));
 }
